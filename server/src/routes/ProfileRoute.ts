39afbefa21a71230235912de637fab0649274519
teshager habtie <teshie00001@gmail.com>
2025-05-20 17:14:01 -0500

    create for status uppdate

--- server/src/routes/ProfileRoute.ts
+++ server/src/routes/ProfileRoute.ts
@@ -6,5 +6,6 @@ router.get("/:userid", profileController.getProfile);
 router.get("/all/allprofile", profileController.allProfile);
 router.post("/", profileController.createProfile);
 router.put("/:userid", profileController.updateProfile);
+router.put("/status/:userid", profileController.updateProfileStatus);
 
 export default router;
